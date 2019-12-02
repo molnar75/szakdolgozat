2296439146a8be8f2d4b61c53b79c2d79a1e0e07
molnar75 <molnar-fanni@gmail.hu>
2019-12-02 20:16:00 +0100

    Fix showing and crop margins

--- szakdoga/first_try.py
+++ szakdoga/first_try.py
@@ -3,14 +3,15 @@ from PIL import Image
 from pdf2image import convert_from_path
 from matplotlib import pyplot as plt
 import numpy as np
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 images = convert_from_path('pdf_files/testpdf.pdf')
 
 numberOfPages = len(images)
 for i in range(numberOfPages): 
-    images[i].save('test' + format(i) + '.png', 'PNG')
+    images[i].save('images/test' + format(i) + '.png', 'PNG')
 
-img = cv2.imread('test0.png')
+img = cv2.imread('images/test0.png')
 
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -23,31 +24,42 @@ intensity_y = img_gray.sum(axis=1) / width
 intensity_x = img_gray.sum(axis=0) / height
 intensity_y = np.flip(intensity_y)
 
-plt.axes([0, 0.15, 1, .8])
-plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
-plt.xticks([])
-plt.yticks([])
-plt.axes([0.3, 0.03, .405, .1])
-plt.xticks([])
-plt.plot(intensity_x)
+fig, ax1 = plt.subplots(figsize=(8,8))
+ax1.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.setp(ax1.set_xticks([]))
+plt.setp(ax1.set_yticks([]))
 
-plt.axes([0.75, 0.15, .1, .8])
-plt.yticks([])
-plt.plot(intensity_y, range(height))
 
-plt.show()
+divider = make_axes_locatable(ax1)
 
-'''
-intensity_y = np.flip(intensity_y)
+ax2 = divider.append_axes("right", size=1, pad=0.1)
+ax2.plot(intensity_y, range(height))
+plt.setp(ax2.set_yticks([]))
 
-ax = plt.axes()
-ax.imshow(img, cmap = 'gray', interpolation = 'bicubic')
-plt.xticks([])
-plt.yticks([])
-ax.plot(intensity_x)
+ax3 = divider.append_axes("bottom", size=1, pad=0.1, sharex=ax1)
+ax3.plot(intensity_x)
 
-plt.yticks([])
-ax.plot(intensity_y, range(height))
 
-plt.show()
-'''
+plt.savefig('img_intensity/test.png')
+
+for i in range(1,width-1):
+    if intensity_x[i] != intensity_x[i-1]:
+        left_margin = i
+        break
+    
+for i in range(width-2,0,-1):
+    if intensity_x[i] != intensity_x[i+1]:
+        right_margin = i
+        break
+    
+for i in range(1,height-1):
+    if intensity_y[i] != intensity_y[i-1]:
+        bottom_margin = i
+        break
+for i in range(height-2,0,-1):
+    if intensity_y[i] != intensity_y[i+1]:
+        top_margin = i
+        break
+
+im1 = test.crop((left_margin, height-top_margin, right_margin,height-bottom_margin))
+im1.save('img_crop_margin/testcrop.png', 'PNG')
