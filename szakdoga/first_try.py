0e32f58e4b1b254a529817fd0a2d96e45e947471
molnar <molnar.fanni@simplesoft.hu>
2020-02-18 08:42:04 +0100

    First try with blob detector

--- szakdoga/first_try.py
+++ szakdoga/first_try.py
@@ -53,12 +53,40 @@ if __name__ == '__main__':
         margins = get.get_margins(width, height, intensity_x, intensity_y)
         crop.crop_margins(i, image, margins, height)
         
-        paragraph_coordinates = get.get_paragraphs(height, intensity_y, margins)
+        paragraph_coordinates = get.get_paragraphs(height, intensity_y)
         crop.crop_paragraphs(i, paragraph_coordinates, image, margins, height)
         
         line_coordinates = get.get_lines(height, intensity_y)
         line_number = crop.crop_lines(i, line_coordinates, image, margins, height)
-        for j in range(line_number):
+        
+        params = cv2.SimpleBlobDetector_Params()
+
+        # Change thresholds
+        params.minThreshold = 1
+        params.maxThreshold = 200
+        
+        
+        # Filter by Area.
+        params.filterByArea = False
+        params.minArea = 1
+        
+        # Filter by Circularity
+        params.filterByCircularity = False
+        params.minCircularity = 0.1
+        
+        # Filter by Convexity
+        params.filterByConvexity = False
+        params.minConvexity = 0.1
+            
+        # Filter by Inertia
+        params.filterByInertia = False
+        params.minInertiaRatio = 0.01
+        ver = (cv2.__version__).split('.')
+        if int(ver[0]) < 3 :
+        	detector = cv2.SimpleBlobDetector(params)
+        else : 
+        	detector = cv2.SimpleBlobDetector_create(params)
+        for j in range(3):
             line_read = cv2.imread('img_crop_lines/' + format(i) + 'page_crop' + format(j) + '.png')
             line_gray = cv2.cvtColor(line_read, cv2.COLOR_BGR2GRAY)
             
@@ -66,17 +94,19 @@ if __name__ == '__main__':
             pix = line.load()
             width = line.size[0]
             height = line.size[1]
-        
+                        
+            keypoints = detector.detect(line_gray)
+            im_with_keypoints = cv2.drawKeypoints(line_gray, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+            cv2.imshow("Keypoints", im_with_keypoints)
+            
             intensity_x = line_gray.sum(axis=0) / height
+            
             words_coordinates = get.get_words(width, intensity_x)
             word_number = crop.crop_words(j,words_coordinates, line, height, width) 
             
             characters_coordinates = get.get_characters(width, intensity_x)
-            character_number = crop.crop_characters(j,characters_coordinates, line, height, width) 
-    # marginal = draw_margins(image, margins)
+            character_number = crop.crop_characters(j,characters_coordinates, line, height, width)
     pass
-#TODO crop first words too
-#TODO find solution for "j" problem
 #TODO separate words with hyphen 
 #TODO separate brackets from words, dot and comma too
 #TODO crop from the top of the page to the bottom
